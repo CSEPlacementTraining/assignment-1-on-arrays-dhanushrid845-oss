a100d8eb2d16ebe5e831b420a9a373f5e1457f76
dhanushrid845-oss <dhanushrid845@gmail.com>
2026-03-11 21:18:21 +0530

    Update Assignment1.java

--- Assignment1.java
+++ Assignment1.java
@@ -4,11 +4,13 @@ class Assignment1 {
         int[] leapYears = new int[15];
         int count = 0;
 
-        while(count < 15) {
-            if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        while (count < 15) {
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
                 leapYears[count] = year;
                 count++;
             }
+
             year++;
         }
 
